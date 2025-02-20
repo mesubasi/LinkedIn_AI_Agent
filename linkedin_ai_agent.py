e0fb67c44555b506c6fea42288ce688028065a89
Muhammed Emin Subaşı <mesub08@gmail.com>
2025-02-20 14:26:37 +0300

    refactor: optimize prompt

--- linkedin_ai_agent.py
+++ linkedin_ai_agent.py
@@ -18,7 +18,6 @@ def extract_pdf_text(file_path):
 
 
 cv_text = extract_pdf_text(cv)
-print(cv_text)
 
 browser = Browser(
     config=BrowserConfig(
@@ -38,16 +37,16 @@ async def main():
                 f"""Search and apply for jobs on LinkedIn continuously until the user stops. The steps should be as follows:
                 1. Log in to LinkedIn. Verify that you are logged in with username and password.
                 2. On the home page or in the navigation bar, find the 'İş İlanları' tab and click on it.
-                3. Search for 'Yazılım Geliştirici' jobs in Istanbul, Türkiye and press the "Arama Yap" button.
+                3. Search for 'Yazılım Geliştirici' jobs in İstanbul, Türkiye and press the "Arama Yap" button or Enter.
                 4. Study each advertisement by reading the titles and short descriptions.
                 5. Read the description of the advertisement and compare it with the content of the CV. CV: {cv_text}
                 If the requirements of the advertisement match the skills and experience on your CV, apply.
                 6. Skip listings where the "Uygula" button appears.
                 7. If the advertisement offers an 'Kolay Başvuru' option and matches your CV, apply immediately.
                 8. Choose the appropriate salary range according to the level of the position (e.g. lower salary range for Junior positions).
-                9. Answer the application questions automatically: multiple choice, short answer and yes/no questions as appropriate.
+                9. Answer the application questions automatically: multiple choice, short answer and yes/no questions as appropriate. Skip already filled questions.
                 10. Find and click the 'Next' button at each step. Then find and click on the 'İncele' button. If the 'Başvuruyu Gönder' button appears, complete the application. If it says “Application submitted”, click the “Bitti” button or "X" button to close the window.
-                11. Work for 1 time.
+                11. Keep applying to apply for different and CV-compatible postings until the user stops.
                 """
             ),
             llm=ChatOpenAI(model="gpt-4o"),
